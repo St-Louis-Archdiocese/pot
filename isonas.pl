780e6912ff147b08122abbd7dcab0f4dfbcdf503
Stefan Adams <stefan@borgia.com>
2015-12-04 10:40:04 -0600

    All Objects, not just Books

--- isonas.pl
+++ isonas.pl
@@ -1,84 +1,15 @@
 our $status = 0;
 
-package Tx;
-use Mojo::Base -base;
-
-has 'book';
-has 'person';
-
-sub reset {
-  my $self = shift;
-  $self->book(undef);
-  $self->person(undef);
-}
-
-package Book;
-use Mojo::Base -base;
-
-has 'barcode';
-has 'title';
-has 'status';
-has 'person';
-has 'checkedout';
-has 'returned';
-
-sub lookup {
-  my $self = shift;
-  printf "Looking up %s\n", $self->barcode;
-  # Parse barcode
-  # select *,(if returned is null, 0, 1) status from books left join tx where id = barcode
-  $self->title("Title");
-  $self->status(++$status%2==0);
-  $self->person("Who");
-  $self->checkedout("CheckedOut");
-  $self->returned("Returned");
-  $self;
-}
-
-sub checkout {
-  my ($self, $tx) = @_;
-  printf "Checkout: %s by %s\n", $self->title, $tx->person->name;
-  # insert into tx (book, person, checkout) values (title, name, now())
-  $tx->reset;
-}
-
-sub Return {
-  my ($self, $tx) = @_;
-  printf "Return: %s\n", $self->title;
-  # update tx set returned=now() where book=book and returned is null
-  $tx->reset;
-}
-
-package Person;
-use Mojo::Base -base;
-
-has 'rfid';
-has 'name';
-has 'id';
-has 'timestamp';
-
-sub parse {
-  my $self = shift;
-  printf "Looking up %s\n", $self->rfid;
-  # Parse rfid
-  $self->name("Name");
-  $self->id("ID");
-  $self->timestamp("Timestamp");
-  $self;
-}
-
-package main;
-use feature 'say';
-use Mojo::IOLoop;
-use Mojo::IOLoop::Stream;
+package Client;
 use Mojo::IOLoop::Client;
 
-if ( $ARGV[0] ) {
-  # Create socket connection
+sub run {
+  my ($class, $message) = @_;
+  return unless $message;
   my $client = Mojo::IOLoop::Client->new;
   $client->on(connect => sub {
     my ($client, $handle) = @_;
-    $handle->write($ARGV[0]);
+    $handle->write($message);
     #...
   });
   $client->on(error => sub {
@@ -93,27 +24,126 @@ if ( $ARGV[0] ) {
   exit;
 }
 
+package Tx;
+use Mojo::Base -base;
+
+has 'object' => sub { Object->new };
+has 'person' => sub { Person->new };
+
+sub reset {
+  my $self = shift;
+  say "Resetting\n";
+  $self->object->barcode('');
+  $self->person->rfid('');
+}
+
+package Object;
+use Mojo::Base -base;
+
+has 'barcode';
+has 'name';
+has 'id';
+has 'status';
+has 'person';
+has 'checkedout';
+has 'returned';
+has 'timestamp';
+
+sub lookup {
+  my $self = shift;
+  return undef unless $self->barcode;
+  printf "Looking up %s\n", $self->barcode;
+  # Parse barcode
+  # select *,(if returned is null, 0, 1) status from objects left join tx where id = barcode
+  $self->name("Object");
+  $self->id("Object ID");
+  $self->status(++$status%2==0);
+  $self->person("Person");
+  $self->checkedout("CheckedOut");
+  $self->returned("Returned");
+  $self->timestamp(time);
+  $self;
+}
+
+sub checkout {
+  my ($self, $tx) = @_;
+  return undef unless $self->barcode;
+  printf "Checking out: %s by %s\n", $self->name, $tx->person->name;
+  # insert into tx (object, person, checkout) values (name, name, now())
+  $tx->reset;
+}
+
+sub Return {
+  my ($self, $tx) = @_;
+  return undef unless $self->barcode;
+  printf "Returning: %s\n", $self->name;
+  # update tx set returned=now() where object=object and returned is null
+  $tx->reset;
+}
+
+sub timeout {
+  my $self = shift;
+  return undef unless $self->barcode;
+  time - $self->timestamp > 15;
+}
+
+package Person;
+use Mojo::Base -base;
+
+has 'rfid';
+has 'name';
+has 'id';
+has 'timestamp';
+
+sub parse {
+  my $self = shift;
+  return undef unless $self->rfid;
+  printf "Looking up %s\n", $self->rfid;
+  # Parse rfid
+  $self->name("Person");
+  $self->id("Person ID");
+  $self->timestamp(time);
+  $self;
+}
+
+sub timeout {
+  my $self = shift;
+  return undef unless $self->rfid;
+  time - $self->timestamp > 15;
+}
+
+package main;
+use feature 'say';
+use Mojo::Pg;
+use Mojo::IOLoop;
+use Mojo::IOLoop::Stream;
+
+Client->run($ARGV[0]);
+
 my $tx = Tx->new;
-Mojo::IOLoop->recurring($ENV{TICK} => sub { warn "tick\n" }) if $ENV{TICK};
 my $readable = Mojo::IOLoop::Stream->new(\*STDIN)->timeout(0);
 
-$readable->on(close => sub {
-  my ($stream) = @_;
-  Mojo::IOLoop->stop;
+Mojo::IOLoop->recurring($ENV{TICK} => sub {
+  printf "Status\n  Barcode: %s\n  RFID: %s\n", $tx->object->barcode, $tx->person->rfid;
+}) if $ENV{TICK};
+Mojo::IOLoop->recurring(1 => sub {
+  $tx->reset if $tx->object->timeout || $tx->person->timeout;
 });
+
+$readable->on(close => sub { Mojo::IOLoop->stop });
    
 $readable->on(read => sub {
   my ($stream, $bytes) = @_;
   Mojo::IOLoop->next_tick(sub{
     my $loop = shift;
     chomp $bytes;
-    $tx->book(my $book = Book->new(barcode => $bytes)->lookup);
-    if ( $book->status ) {
-      printf "Barcode (%s): %s -- last checked out by %s on %s until %s\n", ($book->status?'IN':''), $book->title, $book->person, $book->checkedout, $book->returned;
-      $book->checkout($tx) if $tx->person;
+    my $object = $tx->object->barcode($bytes)->lookup;
+    if ( $object->status ) {
+      printf "Got Barcode: %s -- currently %s, last checked out by %s on %s until %s\n", $object->name, ($object->status?'IN':'OUT'), $object->person, $object->checkedout, $object->returned;
+      $object->checkout($tx) if $tx->person->rfid;
     } else {
-      printf "Barcode (%s): %s -- currently checked out by %s on %s\n", ($book->status?'':'OUT'), $book->title, $book->person, $book->checkedout;
-      $book->Return($tx);
+      printf "Got Barcode: %s -- currently %s, checked out by %s on %s\n", $object->name, ($object->status?'IN':'OUT'), $object->person, $object->checkedout;
+      $object->Return($tx);
     }
   });
 });
@@ -123,9 +153,9 @@ Mojo::IOLoop->server({port => 10001} => sub {
   $stream->on(read => sub {
     my ($stream, $bytes) = @_;
     chomp $bytes;
-    $tx->person(my $person = Person->new(rfid => $bytes)->parse);
+    my $person = $tx->person->rfid($bytes)->parse;
     printf "RFID: %s -- %s\n", $person->name, $person->id;
-    $tx->book->checkout($tx) if $tx->book;
+    $tx->object->checkout($tx) if $tx->object->barcode;
   });
 });
 
