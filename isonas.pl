42556900b3dc016f699b40a9646cb3633190b47c
Stefan Adams <stefan@borgia.com>
2015-12-04 12:16:48 -0600

    Ugh.  Total rewrite.

--- isonas.pl
+++ isonas.pl
@@ -26,48 +26,129 @@ sub run {
 
 package Tx;
 use Mojo::Base -base;
+use Mojo::Loader qw(data_section find_modules load_class);
 
-has 'object' => sub { Object->new };
-has 'person' => sub { Person->new };
+has pg => sub { Mojo::Pg->new };
+has object => sub { Tx::Model::Object->new(tx => shift) };
+has person => sub { Tx::Model::Person->new(tx => shift) };
+
+sub parse {
+  my ($self, $bytes) = @_;
+  chomp $bytes;
+  return undef unless $bytes;
+  printf "Parsing up %s\n", $bytes;
+  for my $module (find_modules 'Tx::Plugin') {
+    my $e = load_class $module;
+    warn qq{Loading "$module" failed: $e} and next if ref $e;
+    if ( my $regex = $module->regex($bytes => $self) ) {
+      printf "%s got %s\n", ref $module, ref $regex;
+      if ( $regex->isa('Tx::Model::Object') ) {
+        if ( $self->object->status ) {
+          printf "Got %s: %s -- currently %s, last checked out by %s on %s until %s\n", ref $module, $self->object->name, ($self->object->status?'IN':'OUT'), $self->object->person, $self->object->checkedout, $self->object->returned;
+          $self->object->checkout if $self->person->id;
+        } else {
+          printf "Got %s: %s -- currently %s, checked out by %s on %s\n", ref $module, $self->object->name, ($self->object->status?'IN':'OUT'), $self->object->person, $self->object->checkedout;
+          $self->object->Return;
+        }
+      } elsif ( $regex->isa('Tx::Model::Person') ) {
+        printf "%s: %s -- %s\n", ref $module, $self->person->name, $self->person->id;
+        $self->object->checkout if $self->object->id;
+      } else {
+        say "Unknown input";
+      }
+      return $regex;
+    }
+  }
+  say "Invalid input";
+  $self;
+}
 
 sub reset {
   my $self = shift;
   say "Resetting\n";
-  $self->object->barcode('');
-  $self->person->rfid('');
+  $self->object(Tx::Model::Object->new);
+  $self->person(Tx::Model::Person->new);
 }
 
-package Object;
+package Tx::Plugin::Person::Isonas;
 use Mojo::Base -base;
 
-has 'barcode';
-has 'name';
+sub regex {
+  my ($self, $bytes, $tx) = @_;
+  if ( $bytes =~ /^<\d+>$/ ) {
+    $tx->person->name("Person");
+    $tx->person->id("Person ID");
+    return $tx->person;
+  } elsif ( 
+  return undef;
+}
+
+package Tx::Plugin::Person::Barcode;
+use Mojo::Base -base;
+
+sub regex {
+  my ($self, $bytes, $tx) = @_;
+  if ( $bytes =~ /^\d+$/ ) {
+    $tx->person->id("Person ID");
+    return $person;
+  }
+  return undef;
+}
+
+package Tx::Plugin::Object::Barcode;
+use Mojo::Base -base;
+
+sub regex {
+  my ($self, $bytes, $tx) = @_;
+  if ( $bytes =~ /^[a-z]+$/ ) {
+    $tx->object->id("Person ID");
+    return $tx->object;
+  }
+  return undef;
+}
+
+package Tx::Model;
+use Mojo::Base -base;
+
+has 'tx';
 has 'id';
+has 'timestamp' => sub { time };
+
+sub timeout {
+  my $self = shift;
+  return undef unless $self->id;
+  time - $self->timestamp > 15;
+}
+
+sub lookup {
+  warn "lookup not implemented\n";
+}
+
+package Tx::Model::Object;
+use Mojo::Base 'Tx:Model';
+
+has 'name';
 has 'status';
-has 'person';
+has 'person' => sub { Tx::Model::Person->new(tx => shift->tx) };
 has 'checkedout';
 has 'returned';
-has 'timestamp';
 
 sub lookup {
   my $self = shift;
-  return undef unless $self->barcode;
-  printf "Looking up %s\n", $self->barcode;
-  # Parse barcode
+  return undef unless $self->id;
+  printf "Looking up %s\n", $self->id;
   # select *,(if returned is null, 0, 1) status from objects left join tx where id = barcode
   $self->name("Object");
-  $self->id("Object ID");
   $self->status(++$status%2==0);
   $self->person("Person");
   $self->checkedout("CheckedOut");
   $self->returned("Returned");
-  $self->timestamp(time);
   $self;
 }
 
 sub checkout {
   my ($self, $tx) = @_;
-  return undef unless $self->barcode;
+  return undef unless $self->id;
   printf "Checking out: %s by %s\n", $self->name, $tx->person->name;
   # insert into tx (object, person, checkout) values (name, name, now())
   $tx->reset;
@@ -75,42 +156,16 @@ sub checkout {
 
 sub Return {
   my ($self, $tx) = @_;
-  return undef unless $self->barcode;
+  return undef unless $self->id;
   printf "Returning: %s\n", $self->name;
   # update tx set returned=now() where object=object and returned is null
   $tx->reset;
 }
 
-sub timeout {
-  my $self = shift;
-  return undef unless $self->barcode;
-  time - $self->timestamp > 15;
-}
+package Tx::Model::Person;
+use Mojo::Base 'Tx::Model';
 
-package Person;
-use Mojo::Base -base;
-
-has 'rfid';
 has 'name';
-has 'id';
-has 'timestamp';
-
-sub parse {
-  my $self = shift;
-  return undef unless $self->rfid;
-  printf "Looking up %s\n", $self->rfid;
-  # Parse rfid
-  $self->name("Person");
-  $self->id("Person ID");
-  $self->timestamp(time);
-  $self;
-}
-
-sub timeout {
-  my $self = shift;
-  return undef unless $self->rfid;
-  time - $self->timestamp > 15;
-}
 
 package main;
 use feature 'say';
@@ -124,7 +179,7 @@ my $tx = Tx->new;
 my $readable = Mojo::IOLoop::Stream->new(\*STDIN)->timeout(0);
 
 Mojo::IOLoop->recurring($ENV{TICK} => sub {
-  printf "Status\n  Barcode: %s\n  RFID: %s\n", $tx->object->barcode, $tx->person->rfid;
+  printf "Status\n  Object: %s\n  Person: %s\n", $tx->object->id, $tx->person->id;
 }) if $ENV{TICK};
 Mojo::IOLoop->recurring(1 => sub {
   $tx->reset if $tx->object->timeout || $tx->person->timeout;
@@ -137,14 +192,7 @@ $readable->on(read => sub {
   Mojo::IOLoop->next_tick(sub{
     my $loop = shift;
     chomp $bytes;
-    my $object = $tx->object->barcode($bytes)->lookup;
-    if ( $object->status ) {
-      printf "Got Barcode: %s -- currently %s, last checked out by %s on %s until %s\n", $object->name, ($object->status?'IN':'OUT'), $object->person, $object->checkedout, $object->returned;
-      $object->checkout($tx) if $tx->person->rfid;
-    } else {
-      printf "Got Barcode: %s -- currently %s, checked out by %s on %s\n", $object->name, ($object->status?'IN':'OUT'), $object->person, $object->checkedout;
-      $object->Return($tx);
-    }
+    $tx->parse($bytes);
   });
 });
    
@@ -153,9 +201,7 @@ Mojo::IOLoop->server({port => 10001} => sub {
   $stream->on(read => sub {
     my ($stream, $bytes) = @_;
     chomp $bytes;
-    my $person = $tx->person->rfid($bytes)->parse;
-    printf "RFID: %s -- %s\n", $person->name, $person->id;
-    $tx->object->checkout($tx) if $tx->object->barcode;
+    $tx->parse($bytes);
   });
 });
 
